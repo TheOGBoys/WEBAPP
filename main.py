47122f0b78805021f464658cac241c5e3016ef54
English Muffin <Alexandru.lib@hotmail.com>
2022-12-18 20:32:13 +0000

    Login & Sign Up + Database

--- main.py
+++ main.py
@@ -2,11 +2,5 @@ from website import create_app
 
 app = create_app()
 
-#AIL INI 17.12.22
-#@app.route("/")
-#def home():
-#    return "this is the home page"
-#AIL END 17.12.22
-
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
