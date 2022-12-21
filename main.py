d0c27d3369652dafb3515640e9e8c9c76a364bbc
Miguel Ângelo Simoes dos Santos <miguel-s-santos@telecom.pt>
2022-12-21 21:54:51 +0000

    Add /fixcode for test with OpenAI API

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from website import create_app
+from dotenv import load_dotenv
+import os
+#openAI
+import openai
+
+def configure():
+    load_dotenv()
 
 app = create_app()
 
+configure()
+#OpenAI Connection
+openai.api_key = os.getenv('OPENAI_API_KEY')
+openai.organization = os.getenv('OPENAI_ORGANIZATION_KEY')
+openai.Model.list()
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
